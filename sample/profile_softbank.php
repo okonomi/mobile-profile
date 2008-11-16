32da4b4fd301ff11d94ba4d6ea073818afc0289b
okonomi <okonomi@224e63c0-5457-0410-989b-907d9cf58824>
2008-11-16 16:12:40 +0000

    SoftBankのサンプル修正。ログイン情報をコマンドライン引数で指定するようにした。
    git-svn-id: http://svn.openpear.org/Mobile_Profile/trunk@113 224e63c0-5457-0410-989b-907d9cf58824

--- sample/profile_softbank.php
+++ sample/profile_softbank.php
@@ -5,10 +5,14 @@ define('BASE', dirname(__FILE__));
 ini_set("include_path", BASE."/..".PATH_SEPARATOR.ini_get("include_path"));
 require_once "Mobile/Profile.php";
 
+if ($argc < 3) {
+    exit;
+}
+
 // MOBILE CREATION(http://creation.mb.softbank.jp/)のログイン情報
 $options = array(
-    'username' => '',
-    'password' => '',
+    'username' => $argv[1],
+    'password' => $argv[2],
 );
 
 $profile = & new Mobile_Profile();
